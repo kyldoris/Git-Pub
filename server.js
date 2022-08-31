8f2462268ca778f225f88f1a0f252b0f0d684d14
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-08-31 00:46:08 -0700

    All tasks done except drinks/food_show error

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // REQUIRE DEPENDENCIES
 const express = require('express');
 const drinks = require("./models/drinks");
+const food = require("./models/food");
+
 // const fruits = require ("./models/fruits");
 // // const veggies = require ("./models/fruit");
 
 
 // INITIALIZE EXPRESS APP
 const app = express();
+
 const port = 3000;
+
 
 // DEFINE OUR ROUTESno
 // const fruits = [
@@ -30,15 +34,36 @@ const port = 3000;
 
 app.get("/drinks/", (req, res) => {
     res.render("drinks_index.ejs", {
-        allDrinks: drinks,
+        allDrinks: drinks, 
       })
     });
 
 
-
-app.get("/drinks/:indexOfFruitsArray", (req, res) => {
- res.send(drinks[req.params.indexOfDrinksArray])
+app.get("/drinks/:index", (req, res) => {
+    res.render("drinks_show.ejs", {
+        drink: drinks[req.params.id]})
 });
+
+
+app.get("/food/", (req, res) => {
+    res.render("food_index.ejs",  {
+        allFood: food, 
+      })
+    });
+
+    app.get("/food/:index", (req, res) => {
+        res.render("food_show.ejs", {
+            foods: food[req.params.id]})
+    });
+    
+
+
+// app.get("/fruits/:indexOfFruitsArray", (req, res) => {
+//     res.render("show.ejs", {
+//       //second param must be an object
+//       fruit: fruits[req.params.indexOfFruitsArray], //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
+//     })
+//   })
 
 // TELL OUR APP TO LISTEN ON PORT...
 app.listen(port, () => {
